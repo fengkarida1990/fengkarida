0c57cdfb8212dc9ab5c41f858c9feb6a6a1edc68
fengkarida <fengjie@youayou.cn>
2014-07-26 12:18:10 +0800

    添加了str

---
+++ Google mapTests/Google_mapTests.m
@@ -0,0 +1,34 @@
+//
+//  Google_mapTests.m
+//  Google mapTests
+//
+//  Created by fengkarida on 14-7-25.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Google_mapTests : XCTestCase
+
+@end
+
+@implementation Google_mapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
